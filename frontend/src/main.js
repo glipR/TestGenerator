8242515a9cf7468b98d33b49bf5dfe413c2734bb
glipR <jgoe0001@student.monash.edu>
2019-10-25 07:07:22 +1100

    Add Vue Mathjax.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueMathjax from 'vue-mathjax'
 
+Vue.use(VueMathjax)
 Vue.config.productionTip = false
 
 new Vue({
